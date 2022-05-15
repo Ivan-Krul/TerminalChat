0183b2767e4ff83430119595fde0b6166038547d
Ivan-Krul <kiwiivanya128@gmail.com>
2022-05-15 23:55:31 +0300

    made a simple templates

--- TerminalChat/TerminalChat.cpp
+++ TerminalChat/TerminalChat.cpp
@@ -1,7 +1,45 @@
 ﻿
 #include <iostream>
+#include <string>
+
+using std::wstring;
+using std::wcout;
+
+class user {
+    wstring Password;
+    uint32_t UserIndex;
+protected:
+    wstring Login;
+public:
+    virtual void registrate(wstring login, wstring password) {
+        Login = login;
+        Password = password;
+    }
+};
+
+class message {
+    uint32_t AuthorIndex;
+protected:
+    wstring Text;
+public:
+    virtual void write(wstring input,uint32_t author_index) {
+        Text = input;
+        AuthorIndex = author_index;
+    }
+
+    wstring text() {
+        return Text;
+    }
+
+    uint32_t author() {
+        return AuthorIndex;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    message msg;
+    msg.write(L"Hello!", 1231);
+
+    wcout << msg.text()<<" by "<<msg.author();
 }
